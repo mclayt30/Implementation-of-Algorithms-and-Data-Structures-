9f8a24ee85945e2c772ef222765e62263bbdae04
mclayt30 <mclayton7586@gmail.com>
2023-03-06 00:26:18 -0500

    yes

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import numpy as np
 from ClassFiles import SortingAlgorithms 
 from ClassFiles import DataStructures 
-    
-
-def findSecondMinimum(self): # NEED TO IMPLEMENT
-    secondMin = -1
-    print(self.nums)
-    
-    return secondMin
-
-        
-        
-      
+          
 size = 10
 startArray = np.random.randint(10, size = (size))
 startArray = list(startArray)
-print(str(startArray) + "\n")
 sort = SortingAlgorithms.Sorting()
+
+print(str(startArray) + "\n")
 sortedArray = sort.mergeSort(startArray)
 print(sortedArray)
